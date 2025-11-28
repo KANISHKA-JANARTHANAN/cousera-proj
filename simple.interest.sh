c16b23e570ea106e2c4563853d72416cbbc5284e
KANISHKA J <kanishkajanarthanan2006@gmail.com>
2025-11-28 19:57:17 +0530

    Update simple.interest.sh

--- simple.interest.sh
+++ simple.interest.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+# Simple Interest Calculator Script
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <Principal> <Time> <Rate>"
+    exit 1
+fi
+
+P=$1
+T=$2
+R=$3
+
+SI=$((P * T * R / 100))
+
+echo "Simple Interest = $SI"
 
